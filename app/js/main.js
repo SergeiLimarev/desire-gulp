0e29ea0ac6a0bb67f8b315b4a0c9d5a796113ca7
SergeiLimarev <veritynity@yandex.ru>
2022-01-22 23:52:35 +0600

    📦 Added right-side component

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
 
-})
+    $('.header__btn').on('click', function () {
+        $('.right-side-menu').removeClass('right-side-menu--close');
+    });
+    $('.right-side-menu__close').on('click', function () {
+        $('.right-side-menu').addClass('right-side-menu--close');
+    });
+
+});
